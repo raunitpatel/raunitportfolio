993a02eeb302010494e63777167deb1c7ae1b2b0
raunitpatel <raunit@iitg.ac.in>
2024-07-02 23:03:36 +0530

    added files

--- style.js
+++ style.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing",{
-		strings: ["Engineer", "Coding Enthusiast", " Front End Developer"],
+		strings: ["Student", "AI/ML Enthusiast", "Web Developer Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Engineer", "Coding Enthusiast", "Front End Developer"],
+        strings: ["Student", "AI/ML Enthusiast", "Web Developer Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
